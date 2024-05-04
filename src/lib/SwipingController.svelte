1941d49981951aab66e5d8cf83305db865fc163d
Nicolai Grymer <pronicoxd@gmail.com>
2024-05-04 11:05:29 +0200

    Gracefully handle no more cards

--- src/lib/SwipingController.svelte
+++ src/lib/SwipingController.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import SwipableEvent from './SwipableEvent.svelte';
 	import Event from './event/Event.svelte';
+	import NoMoreEvents from './event/NoMoreEvents.svelte';
 	import { eventSystem, react } from './events';
 
 	const reactedEventIds = Array.from($eventSystem.reactions.keys());
-	const events = $eventSystem.events.filter(
-		(event) => reactedEventIds.includes(event.id) === false
-	);
+	const events = $eventSystem.events
+		.filter((event) => reactedEventIds.includes(event.id) === false)
+		.filter((_, i) => i < 2);
 	let index = 0;
 	$: currentEvent = events[index];
 	$: nextEvent = events[index + 1];
@@ -30,8 +31,16 @@
 </script>
 
 <div class="h-full w-full relative">
-	<SwipableEvent event={currentEvent} on:heart={heart} on:like={like} on:pass={pass} />
+	{#if currentEvent}
+		<SwipableEvent event={currentEvent} on:heart={heart} on:like={like} on:pass={pass} />
+	{:else}
+		<NoMoreEvents />
+	{/if}
 	<div class="absolute pointer-events-none -z-10 top-0 left-0 w-full h-full">
-		<Event event={nextEvent} />
+		{#if nextEvent}
+			<Event event={nextEvent} />
+		{:else if currentEvent}
+			<NoMoreEvents />
+		{/if}
 	</div>
 </div>
